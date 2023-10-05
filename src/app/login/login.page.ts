9b88015a57c401f30c7125099639102be9d57e92
Diego.aml19@gmail.com <diego.aml19@gmail.com>
2023-10-04 22:40:55 -0300

    nuevo login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -16,66 +16,35 @@ import { HelperService } from 'src/app/services/helper.service';
 })
 export class LoginPage implements OnInit {
 
-  formularioLogin: FormGroup;
-
-  email:string = "";
-  contrasena:string = "";
-
-
-  constructor(public fb: FormBuilder,
+  constructor(
+    public formBuilder: FormBuilder,
     public alertController: AlertController,
-    private router:Router,private helperService:HelperService) { 
+    private router:Router,private helperService:HelperService
+    ) {}
 
-    this.formularioLogin = this.fb.group({
-      'nombre': new FormControl("", Validators.required),
-      'password': new FormControl("", Validators.required)
-    })
-  }
+  form = this.formBuilder.group({
+    email: ['',[Validators.email, Validators.required]],
+    password: ['',[Validators.required]]
+  })
+
+
+
 
   ngOnInit() {
   }
 
+  navegarRegistro() {
+    this.router.navigate(['/registro'])
+  }
+  
   login(){
-    if (this.email == "") {
-      //alert("Debe ingresar un email.");
-      this.helperService.showAlert("Debe ingresar un email", "Advertencia");
-      return;
+    if(this.form.valid) {
+      const { email, password } = this.form.getRawValue();
+      
+
+
     }
-    if (this.contrasena == "") {
-      alert("Debe ingresar una contraseña.")
-      return;
-    }
-    
-    if (this.email == "pgy4121-002d" && this.contrasena == "pgy4121-002d") {
-      //alert("Login correcto.");
-      this.router.navigateByUrl("bienvenido");
-    }else{
-      alert("Credeneciales no validas.");
-    }
+
     
   }
-
-  /*meo corte compa;ero no funca 
-  async ingresar(){
-    var f = this.formularioLogin.value;
-
-    var usuario = localStorage.getItem('usuario');
-
-    if(usuario !== null){
-      var user = JSON.parse(usuario);
-      if (user.nombre == f.nombre && user.password == f.password) {
-        console.log('inicio sesion mi xan');
-        localStorage.setItem('inicio sesion mi xan', 'true');
-      }else {
-        const alert = await this.alertController.create({
-          header: 'Datos incorrectos',
-          message: 'Tienes que llenar todos los datos',
-          buttons: ['Aceptar'],
-        });
-        await alert.present();
-      }
-    } else {
-      // Manejo de caso cuando no se encuentra el valor en localStorage
-    }
-  }*/
 } 
